b4a288910e3d63a68143a6763848d0b7d32fdd18
Arturo <da.rodriguezh@uniandes.edu.co>
2022-01-25 22:47:02 -0500

    Testsss

--- main.cpp
+++ main.cpp
@@ -32,8 +32,33 @@ int main(int argc, char const *argv[])
     std::vector<std::string> types = {"C2V", "C2V", "T2V", "T1V", "C1V"};
 
     FEM::Geometry *geometria = new FEM::Geometry(coords, dicts, types, 1);
+    std::vector<std::vector<double>> cb = {{0.0, 0.1}, {1.0, 1.1}};
+    geometria->setEbc(cb);
     FEM::Core O = FEM::Core(geometria);
     std::cout << "=========================================" << std::endl;
+
+    for (int k = 0; k < 4; k++)
+    {
+        FEM::Element *e = O.geometry->elements[k];
+        for (int i = 0; i < e->n; i++)
+        {
+            for (int j = 0; j < e->n; j++)
+            {
+                e->Ke(i, j) = 1.0;
+            }
+        }
+        // std::cout << *e << std::endl;
+        // std::cout << "--------" << std::endl;
+        // std::cout << e->Ke << std::endl;
+    }
+    O.ensembling();
+    O.borderConditions();
+    O.solveES();
+    std::cout << O.K << std::endl;
+    std::cout << O.S << std::endl;
+    std::cout << O.U << std::endl;
+
+    int a = 0;
 
     return 0;
 }
